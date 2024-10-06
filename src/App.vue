63166186f1b300f3c24b8fbabccf548ab594eec1
MatveyVue <shabanov.matveyka@bk.ru>
2024-10-06 16:22:23 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+<div style="margin-left: 15px">
+<h1 style="font-family: Geologica; color: white;">Marketplace:</h1>
+<input style="font-family: Geologica;" placeholder="Search..."><br>
+<button class="job"><p style="font-family: Geologica;">Jobs</p></button>
+<button class="peop"><p style="font-family: Geologica;">People</p></button>
+</div>
+
+<div>
+  
+</div>
+</template>
+
+<style>
+body {
+  background-color: #101622;
+}
+
+input {
+  background-color: #272e38;
+  border-radius: 25px;
+  height: 35px;
+  width: 400px;
+  border-color: white;
+  border-width: 1px;
+}
+
+.job {
+  background-color: #97f492;
+  width: 180px;
+  height: 35px;
+  border-radius: 25px;
+  border-color: white;
+  border-width: 1px;
+  margin-top: 15px;
+}
+
+.peop {
+  background-color: #272e38;
+  width: 180px;
+  height: 35px;
+  border-radius: 25px;
+  border-color: white;
+  border-width: 1px;
+  margin-top: 15px;
+  margin-left: 40px;
+}
+</style>
